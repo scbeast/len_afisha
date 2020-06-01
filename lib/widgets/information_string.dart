0b450c12bd644187b0a29f0554eff7d221f11fad
scbeast <scbeast555655@gmail.com>
2020-06-01 17:22:07 +1000

    refuctoring

---
+++ lib/widgets/information_string.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class InformationString extends StatelessWidget {
+  const InformationString(
+      {Key key,
+      @required this.title,
+      @required this.caption,
+      @required this.launchArgument})
+      : super(key: key);
+
+  final String title;
+  final String caption;
+  final String launchArgument;
+
+  Future<bool> _launchAction() async {
+    await launch(launchArgument);
+    return true;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text(
+          '$title',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        GestureDetector(
+          child: Text(
+            '$caption',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Colors.blue,
+            ),
+          ),
+          onTap: _launchAction,
+        ),
+      ],
+    );
+  }
+}
